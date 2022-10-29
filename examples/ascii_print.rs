fdb0b63745ff54e007b545e3723289f07ce281b1
Scott Steele <scottlsteele@gmail.com>
2022-10-29 13:47:28 -0400

    feat(ascii): option to print non-compact

--- examples/ascii_print.rs
+++ examples/ascii_print.rs
@@ -3,7 +3,8 @@ use std::{
     io::{BufRead, BufReader},
 };
 
-// TODO simplify input reading now that we've moved parsing to the lib
+use knotty::AbbreviatedDiagram;
+
 fn read_input(file: Option<String>) -> Result<String, String> {
     let lines = match file.as_deref() {
         None | Some("-") => std::io::stdin()
@@ -21,16 +22,21 @@ fn read_input(file: Option<String>) -> Result<String, String> {
 }
 
 fn main() -> Result<(), String> {
-    let knot = read_input(std::env::args().nth(1))?.parse::<knotty::AbbreviatedDiagram>()?;
-    let display = if matches!(
-        var("KNOTTY_GRID").ok().as_deref(),
-        None | Some("" | "0" | "false")
-    ) {
-        knot.ascii_print_compact::<false>()
-    } else {
-        knot.ascii_print_compact::<true>()
+    let knot = read_input(std::env::args().nth(1))?.parse::<AbbreviatedDiagram>()?;
+
+    let grid = var("KNOTTY_GRID").ok().as_deref() == Some("true");
+    let compact = var("KNOTTY_COMPACT").ok().as_deref() == Some("true");
+
+    #[rustfmt::skip]
+    let func = match (compact, grid) {
+        (true,  true)  => AbbreviatedDiagram::ascii_print_compact::<true>,
+        (true,  false) => AbbreviatedDiagram::ascii_print_compact::<false>,
+
+        (false, true)  => AbbreviatedDiagram::ascii_print::<true>,
+        (false, false) => AbbreviatedDiagram::ascii_print::<false>,
     };
 
+    let display = func(&knot);
     print!("{display}");
 
     Ok(())
